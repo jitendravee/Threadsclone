94d5833a945f0c1f39b814c35a81e7e2f6aa710b
jitendravee <jitendravee098@gmail.com>
2023-09-16 22:47:38 +0530

    third

--- app/(root)/components/shared/Topbar.tsx
+++ app/(root)/components/shared/Topbar.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn } from "@clerk/nextjs";
 function Topbar() {
-    return <h1>Top</h1>
+    return(
+     <nav className="topbar">
+<Link href ="/" className = "flex items-center  gap-4">
+<Image src = "/assets/logo.svg" alt = "logo" width = {28} height={28}   />    
+<p className = "text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+</Link>
+<div className = "flex items-center gap-1">
+    <div className = "block md:hidden">
+        <SignedIn></SignedIn>
+    </div>
+</div>
+
+     </nav>
+    )
 }
 
 export default Topbar;
